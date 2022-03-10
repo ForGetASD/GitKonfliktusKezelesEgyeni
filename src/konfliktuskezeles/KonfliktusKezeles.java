8eb23f03a2283fc5befd13dc991d9541332306c3
ForGetASD <vitayz60@gmail.com>
2022-03-10 12:40:18 +0100

    3. név deklarálása

--- src/konfliktuskezeles/KonfliktusKezeles.java
+++ src/konfliktuskezeles/KonfliktusKezeles.java
@@ -3,7 +3,9 @@ package konfliktuskezeles;
 public class KonfliktusKezeles {
 
     public static void main(String[] args) {
+        int i = 2;
         String nev1 = "VZ";
         String nev2 = "X.Y.";
+        int nev3;
     }
 }
